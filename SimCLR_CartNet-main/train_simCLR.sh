8ba283609a1f2c811901174b2410185465500879
LauraSola <104486393+LauraSola@users.noreply.github.com>
2025-06-19 17:36:22 +0200

    Delete SimCLR_CartNet-main directory

--- SimCLR_CartNet-main/train_simCLR.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p gpi.compute            # Partition to submit to
-#SBATCH --mem=20G 
-#SBATCH -c6     # Max CPU Memory
-#SBATCH --gres=gpu:1,gpumem:11G
-#SBATCH --time=1:00:00
-
-
-python run.py 
